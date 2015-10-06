e3d93c23995b22541843e8e68ff00f5f2fb7d49f
Daniel Lewan <vision360.daniel@gmail.com>
2015-10-06 22:39:46 +0200

    Second iterationn
    N plantes working
    better planet class
    Player has a script (not instance script as before)

---
+++ scenes/Planet2.gd
@@ -0,0 +1,12 @@
+#new version of planet class
+extends Sprite
+
+var radius = 160.0               # size of planet and collider
+export(float) var mass   = 1000  # mass (gravity)
+export(float) var period = 20    # rotation
+
+func _ready():
+	if get_scale() != Vector2(1,1):
+		radius = get_scale().x * radius
+		prints(radius)
+	get_node("../Player").planets.append(self)
---
+++ scenes/Player2.gd
@@ -0,0 +1,42 @@
+extends KinematicBody2D
+
+var velocity = Vector2()
+var planets = []
+
+func _fixed_process(delta):
+	var force = Vector2()
+	#TEMP
+	if planets != []:
+		for p in planets:
+			var distance = p.get_pos().distance_squared_to(get_pos())
+			force += p.mass * 2 * (p.get_pos() - get_pos()) / distance * delta
+	
+	pass
+#    var p = get_node("../Planet").get_pos()
+#    var v = p - get_pos()
+#    velocity.y += delta * v.y
+#    velocity.x += delta * v.x
+#    #rotate right (clockwise)        #right
+#    var v_right = Vector2(-v.y,v.x)
+#    #rotate left (counter-clockwise) #left
+#    var v_left = Vector2(v.y,-v.x)
+#    if Input.is_action_pressed("ui_left"):
+#        velocity += delta * v_right * 0.5
+#    if Input.is_action_pressed("ui_right"):
+#        velocity += delta * v_left * 0.5
+#
+#
+#	velocity += delta * v * 3
+	velocity += force * delta * 100
+	var motion = velocity * delta
+	set_rot( force.atan2() )
+	move( motion )  
+#    
+	if (is_colliding()):
+		var n = get_collision_normal()
+		motion = n.slide( motion ) 
+		velocity = n.slide( velocity )
+		move( motion * 0.5 )
+
+func _ready():
+	set_fixed_process(true)
