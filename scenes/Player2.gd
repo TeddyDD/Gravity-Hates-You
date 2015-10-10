9115759b160984f14d57b9dfddb99586fd104110
Daniel Lewan <vision360.daniel@gmail.com>
2015-10-10 17:37:16 +0200

    Working animations

--- scenes/Player2.gd
+++ scenes/Player2.gd
@@ -4,6 +4,8 @@ var velocity = Vector2()
 var planets = []
 const MAX_HZ_SPEED = 100
 var h_vel = Vector2()
+var animation
+var is_input = false
 
 func _fixed_process(delta):
 	var force = Vector2()
@@ -23,15 +25,37 @@ func _fixed_process(delta):
 		var n = get_collision_normal()
 		motion = n.slide( motion ) 
 		velocity = n.slide( velocity )
+		is_input = false
+		#go left
 		if Input.is_action_pressed("ui_left"):
+			is_input = true
 			h_vel = Vector2(n.y, -n.x) * 1000
 			velocity += h_vel * delta
+			if animation.get_current_animation() != "run": 
+				animation.play("run")
+			get_node("Sprite").set_flip_h( true )
+		#go right
 		if Input.is_action_pressed("ui_right"):
+			is_input = true
 			h_vel = Vector2(-n.y, n.x) * 1000
 			velocity += h_vel * delta
+			
+			#animation
+			if animation.get_current_animation() != "run": 
+				animation.play("run")
+			get_node("Sprite").set_flip_h( false )
+		#jump
 		if Input.is_action_pressed("ui_select"):
 			velocity += -force * 200
+		#no input but coliding
+		if not is_input:
+			animation.play("idle")
 		move( motion * 0.2)
+	else: #not coliding
+		animation.play("idle")
+		is_input = false
 
 func _ready():
+	animation = get_node("AnimationPlayer")
+	animation.play("idle")
 	set_fixed_process(true)
