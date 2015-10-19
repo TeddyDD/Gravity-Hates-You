4d01203a34c73f1f83323dec567b9369e82238c9
Daniel Lewan <vision360.daniel@gmail.com>
2015-10-19 20:53:32 +0200

    Added levels

--- scenes/global.gd
+++ scenes/global.gd
@@ -5,7 +5,16 @@ var music_pos = 1
 var current_level # path to scene
 var level_data = [
 	{"name" :"Tutorial", "file": "res://levels/tutorial.xml", "time": null},
-	{"name" :"First steps", "file": "res://scenes/level.xml", "time": null},
+	{"name" :"Tutorial - Jumping", "file": "res://levels/tutorial2.xml", "time": null},
+	{"name" :"Give it to me", "file": "res://levels/level1.xml", "time": null},
+	{"name" :"Bigger is Better", "file": "res://levels/level2.xml", "time": null},
+	{"name" :"I said nothing", "file": "res://levels/level3.xml", "time": null},
+	{"name" :"666?", "file": "res://levels/level4.xml", "time": null},
+	{"name" :"Catch 'Em All", "file": "res://levels/level8.xml", "time": null},
+	{"name" :"Be happy", "file": "res://levels/level6.xml", "time": null},
+	{"name" :"That was Test level", "file": "res://levels/test_level.xml", "time": null},
+	{"name" :"Be quick or stuck", "file": "res://levels/level7.xml", "time": null},
+	{"name" :"Fly Adam, fly!", "file": "res://levels/level5.xml", "time": null},
 ]
 
 func _ready():
